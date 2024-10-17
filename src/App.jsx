df760f0794a4c4c47cc09897c7bcb0bf16263c73
nickbhola99 <bholaphillipn@gmail.com>
2024-10-17 12:08:26 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ProjectsPage from "./pages/ProjectsPage";
+import NavBar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProjectDetails from "./pages/ProjectDetails";
+import "./App.css";
+
+function App() {
+  const [projects, setProjects] = useState(null);
+
+  // fetch all project when component first render
+  useEffect(() => {
+    // fetch all projects
+    const fetchProjects = async () => {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/projects/`);
+      const projectsData = await res.json();
+      console.log(projectsData);
+      setProjects(projectsData.projects);
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        
+        <Route
+          path="/projects"
+          element={
+            <ProjectsPage projects={projects} setProjects={setProjects} />
+          }
+        />
+
+          <Route path="/projects/:id" element={<ProjectDetails projects={projects} setProjects={setProjects}/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
